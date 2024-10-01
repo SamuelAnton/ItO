0f7feeb3155cbf29f39c75fd47fc74e46e864fcc
Damir <damir.sadykov0407@gmail.com>
2024-10-01 15:18:46 +0300

    Done Damir's part

--- Optimization1/src/Simplex.java
+++ Optimization1/src/Simplex.java
@@ -9,20 +9,49 @@ public class Simplex {
         //Artyom
     }
 
-    private boolean isFinished(double[] C, double[][] A, double[] b){
-        //TODO
-        //Damir
-        return false;
+    private boolean isFinished(double[] C){
+        for (double d: C){
+            if (d < 0){
+                return false;
+            }
+        }
+        return true;
     }
 
-    private boolean isCorrectInput(double[] C, double[][] A, double[] b, int eps){
-        //TODO
-        //Damir
+    private boolean isCorrectInput(double[] b){
+        for (double d: b){
+            if (d < 0){
+                System.out.println("The method is not applicable!");
+                return false;
+            }
+        }
         return true;
+    }
+
+    // return:  index of leaving variable in C or -1 if problem is unbounded
+    // input:   index - index of entering variable in C
+    private int getLeavingVariable(int index, double[][] A, double[] b){
+        int leavingIndex = -1;
+        double minRation = 0;
+        for (int i = 0; i < b.length; i++){
+            if (b[i] > 0){
+                if (minRation == 0 || minRation > b[i] / A[i][index]){
+                    minRation = b[i] / A[i][index];
+                }
+            }
+        }
+        return leavingIndex;
     }
 
     private void addSlack(double[] C, double[][] A){
         //TODO
         //Artyom
+    }
+
+    private void output(double[] x, double value){
+        for (int i = 1; i < x.length + 1; i++){
+            System.out.println("x" + i + " = " + x[i - 1]);
+        }
+        System.out.println("Optimized value of objective function: " + value);
     }
 }
