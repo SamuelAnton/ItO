4ce54a372700cb45442a10c0ec1ddb35540ebddc
Damir <damir.sadykov0407@gmail.com>
2024-10-01 13:01:33 +0300

    problem separation

---
+++ Optimization1/src/Simplex.java
@@ -0,0 +1,28 @@
+public class Simplex {
+    public void maximize(double[] C, double[][] A, double[] b, int eps){
+        //TODO
+        //Artyom
+    }
+
+    public void minimize(double[] C, double[][] A, double[] b, int eps){
+        //TODO
+        //Artyom
+    }
+
+    private boolean isFinished(double[] C, double[][] A, double[] b){
+        //TODO
+        //Damir
+        return false;
+    }
+
+    private boolean isCorrectInput(double[] C, double[][] A, double[] b, int eps){
+        //TODO
+        //Damir
+        return true;
+    }
+
+    private void addSlack(double[] C, double[][] A){
+        //TODO
+        //Artyom
+    }
+}
