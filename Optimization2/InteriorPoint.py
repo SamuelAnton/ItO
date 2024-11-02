d501342eb98f67d709e70ec2184465170fcabf05
Damir <damir.sadykov0407@gmail.com>
2024-11-02 22:05:25 +0300

    fix bug with exponential value print

--- Optimization2/InteriorPoint.py
+++ Optimization2/InteriorPoint.py
@@ -16,9 +16,10 @@ class InteriorPoint:
             x_ = add(ones(len(A_[0])), cp * (alpha / v))
             x = dot(D, x_)
 
+
             if linalg.norm(subtract(prevX, x), ord=2) < eps:
                 for i in range(len(x)):
-                    x[i] = round(x[i], len(str(eps)) - 2)
+                    x[i] = "{:.2f}".format(round(x[i], len(str(eps)) - 2))
                 return x
                 break
     
@@ -28,8 +29,18 @@ class InteriorPoint:
 
 
 if __name__ == "__main__":
-    x = array([0.5, 3.5, 1, 2], float)
-    A = array([[2, 4, 1, 0], [1, 3, 0, -1]], float)
-    c = array([1, 1, 0, 0])
-    i = InteriorPoint()
-    print(i.minimize(A=A, c=c, b=array([]), alpha=0.5, eps=0.0001, x=x))
+    ip = InteriorPoint()
+    eps = 0.001
+
+    # Test 1
+    print("Test 1:")
+    C1 = array([9, 10, 16, 0, 0, 0], float)
+    A1 = array([
+        [18, 15, 12, 1, 0, 0],
+        [6, 4, 8, 0, 1, 0],
+        [5, 3, 3, 0, 0, 1]
+    ], float)
+    b1 = array([360, 192, 180], float)
+    x1_initial = array([1.0, 1.0, 1.0, 315, 174, 169], float)
+    result1 = ip.maximize(A=A1, c=C1, b=b1, alpha=0.5, eps=eps, x=x1_initial)
+    print(f"Interior-Point Solution for Test 1: x* = {result1}")    
